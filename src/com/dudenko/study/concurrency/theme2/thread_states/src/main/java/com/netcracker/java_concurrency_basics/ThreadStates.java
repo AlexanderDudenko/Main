a5a33675ddbe69aeae5b543bb76e59d30059bec9
AlexanderDudenko <alfa8076@yandex.ru>
2018-03-21 00:51:55 +0400

    added displaying NEW and WAIT Thread states (lab2 partial solution)

--- src/com/dudenko/study/concurrency/theme2/thread_states/src/main/java/com/netcracker/java_concurrency_basics/ThreadStates.java
+++ src/com/dudenko/study/concurrency/theme2/thread_states/src/main/java/com/netcracker/java_concurrency_basics/ThreadStates.java
@@ -2,17 +2,59 @@ package com.dudenko.study.concurrency.theme2.thread_states.src.main.java.com.net
 
 public class ThreadStates {
     public static void main(String[] args) {
+        printNewState();
+        printRunnableState();
+        printBlockedState();
+    }
+
+    private static void printNewState() {
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                // Implement.
             }
+
         };
 
         Thread t = new Thread(r);
+        System.out.println(t.getState());
+    }
 
-        // Implement.
+    private static void printRunnableState() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+            }
 
+        };
+
+        Thread t = new Thread(r);
+        t.start();
+        System.out.println(t.getState());
+    }
+
+    private static void printBlockedState() {
+
+
+        Runnable r = new Runnable() {
+            private Object object = new Object();
+
+            private synchronized void customWait() {
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void run() {
+                customWait();
+            }
+
+        };
+
+        Thread t = new Thread(r);
+        t.start();
         System.out.println(t.getState());
     }
 }
