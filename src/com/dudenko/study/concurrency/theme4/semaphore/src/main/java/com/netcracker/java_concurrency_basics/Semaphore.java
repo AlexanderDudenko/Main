bb6d2788df3beca2f03e6b8f1f2a38cfe6b607e6
AlexanderDudenko <alfa8076@yandex.ru>
2018-03-22 00:09:33 +0400

    added solution of lab3 (java concurrency basic)

--- src/com/dudenko/study/concurrency/theme4/semaphore/src/main/java/com/netcracker/java_concurrency_basics/Semaphore.java
+++ src/com/dudenko/study/concurrency/theme4/semaphore/src/main/java/com/netcracker/java_concurrency_basics/Semaphore.java
@@ -1,35 +1,76 @@
 package com.dudenko.study.concurrency.theme4.semaphore.src.main.java.com.netcracker.java_concurrency_basics;
 
 class Semaphore {
-    private final int permits;
+    //    private final int permits;
+    private int permits;
 
     public Semaphore(int permits) {
         this.permits = permits;
     }
 
     public void acquire() {
-        // Implement.
+        if (permits == 0) {
+            System.out.println(Thread.currentThread().getName() + " couldn't acquire semaphore");
+        } else {
+            synchronized (this) {
+                --permits;
+                System.out.println(Thread.currentThread().getName() + " acquire semaphore");
+
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public void release() {
-        // Implement.
+        synchronized (this) {
+            if (permits < 3) {
+                ++permits;
+            }
+            notifyAll();
+        }
+        System.out.println(Thread.currentThread().getName() + " release semaphore");
     }
 
     public static void main(String[] args) {
         // Run 10 threads that use the semaphore.
         // The group of 3 threads should be able to execute the critical section.
-        Semaphore semaphore = new Semaphore(3);
+        final Semaphore semaphore = new Semaphore(3);
 
-        try {
-            semaphore.acquire();
 
-            System.out.println("Permitted.");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    semaphore.acquire();
 
-            Thread.sleep(2000);
+//                    System.out.println("Permitted.");
 
-            semaphore.release();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+                    Thread.sleep(2000);
+
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+        for (int i = 0; i < 10; ++i) {
+            Thread thread = new Thread(runnable);
+
+            // for pretty demo
+            if (i == 5) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            thread.start();
         }
     }
 }
